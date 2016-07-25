f4e312bf3a27116576f6db7bee3fde3ec5d8c8ce
alonoslav <vadym.vorobel@gmail.com>
2016-07-25 07:39:26 +0300

    added first route

--- client/main.js
+++ client/main.js
@@ -1,1 +1,1 @@
-import './main.html';
+import '/imports/startup/client';
--- imports/startup/client/router.js
+++ imports/startup/client/router.js
@@ -1,2 +1,16 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
+import React from 'react';
+import { mount } from 'react-mounter';
 
+import { FlowRouter } from 'meteor/kadira:flow-router'
+
+import BlankLayout from '/imports/ui/layouts/BlankLayout';
+import MainLayout from '/imports/ui/layouts/MainLayout';
+
+import Dashboard from '/imports/ui/components/Dashboard';
+
+FlowRouter.route('/', {
+  name: 'dashboard',
+  action() {
+    mount(BlankLayout, {content: <Dashboard />});
+  }
+});
