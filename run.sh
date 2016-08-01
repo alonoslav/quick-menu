934153a50fdf7e920576a33b6bf1a128eeb096a7
alonoslav <vadym.vorobel@gmail.com>
2016-08-01 13:14:52 +0300

    removed run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if uname="Darwin" ; then
-  METEOR_DEBUG_SPRINGBOARD=true ~/.meteor/packages/meteor-tool/1.4.0-1-rc.1/mt-os.osx.x86_64/meteor run -p 3333 --settings settings.json
-else
-  meteor run -p 3333 --settings settings.json
-fi
